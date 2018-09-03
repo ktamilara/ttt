224c3ba7d88ce896cf7231d95b3ba0691968036c
ktamilara <42666475+ktamilara@users.noreply.github.com>
2018-09-03 12:26:22 +0530

    Create ss.java

---
+++ ss.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class ss {
+
+    public static void main(String[] args) {
+
+        int count = 0, num = 3452;
+
+        while(num != 0)
+        {
+            // num = num/10
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
